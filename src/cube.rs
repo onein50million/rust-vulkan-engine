95fdfe2d1c4d998e8c8a60d6367763f0ebb043e5
Daniel Wart <dman.wart@gmail.com>
2021-07-18 15:23:25 -0700

    Revert "still have seams with separate vertex arrays for each face"
    This reverts commit 160d5afa

--- src/cube.rs
+++ src/cube.rs
@@ -1,404 +1,56 @@
+use cgmath::{Vector3,Vector2};
 use crate::support::Vertex;
-use cgmath::{Vector2, Vector3};
 
-/*
 
-   0   3
-
-   1   2
-
-4   7
-
-5   6
-
-*/
-
-pub(crate) const POSITIVE_X_VERTICES: [Vertex; 4] = [
+pub(crate) const SKYBOX_BACKPLANE_VERTICES: [Vertex;4] = [
     Vertex {
+        position: Vector3 {
+            x: -1.0,
+            y: -1.0,
+            z: 0.0,
+        },
+        color: Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0
+        },
+        texture_coordinate: Vector2 { x: 0.0, y: 0.0 }
+    }, Vertex {
+        position: Vector3 {
+            x: -1.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        color: Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0
+        },
+        texture_coordinate: Vector2 { x: 0.0, y: 1.0 }
+    }, Vertex {
+        position: Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        color: Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0
+        },
+        texture_coordinate: Vector2 { x: 1.0, y: 1.0 }
+    }, Vertex {
         position: Vector3 {
             x: 1.0,
             y: -1.0,
-            z: -1.0,
+            z: 0.0,
         },
         color: Vector3 {
             x: 1.0,
             y: 1.0,
-            z: 1.0,
+            z: 1.0
         },
-        texture_coordinate: Vector2 { x: 0.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 1.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 {x: 1.0/6.0, y: 0.0},
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: -1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 0.0, y: 0.0 },
-    },
+        texture_coordinate: Vector2 { x: 1.0, y: 0.0 }
+    }
 ];
-
-
-pub(crate) const NEGATIVE_X_VERTICES: [Vertex; 4] = [
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: -1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 2.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 1.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: 1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 1.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: -1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 {
-            x: 2.0 / 6.0,
-            y: 1.0,
-        },
-    },
-];
-
-
-
-pub(crate) const POSITIVE_Y_VERTICES: [Vertex; 4] = [
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 3.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 3.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: 1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 2.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 2.0/6.0, y: 1.0 },
-    },
-];
-
-pub(crate) const NEGATIVE_Y_VERTICES: [Vertex; 4] = [
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: -1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 3.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: -1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 3.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: -1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 4.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: -1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 4.0/6.0, y: 0.0 },
-    },
-];
-
-
-
-pub(crate) const POSITIVE_Z_VERTICES: [Vertex; 4] = [
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: -1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 5.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 5.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 4.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: -1.0,
-            z: 1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 4.0/6.0, y: 1.0 },
-    },
-];
-
-pub(crate) const NEGATIVE_Z_VERTICES: [Vertex; 4] = [
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: -1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 5.0/6.0, y: 0.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: -1.0,
-            y: 1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 5.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 6.0/6.0, y: 1.0 },
-    },
-    Vertex {
-        position: Vector3 {
-            x: 1.0,
-            y: -1.0,
-            z: -1.0,
-        },
-        color: Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        },
-        texture_coordinate: Vector2 { x: 6.0/6.0, y: 0.0 },
-    },
-];
-
-
-pub(crate) const CUBE: [Vertex; 24] = [
-    POSITIVE_X_VERTICES[0],
-    POSITIVE_X_VERTICES[1],
-    POSITIVE_X_VERTICES[2],
-    POSITIVE_X_VERTICES[3],
-
-    NEGATIVE_X_VERTICES[0],
-    NEGATIVE_X_VERTICES[1],
-    NEGATIVE_X_VERTICES[2],
-    NEGATIVE_X_VERTICES[3],
-
-    POSITIVE_Y_VERTICES[0],
-    POSITIVE_Y_VERTICES[1],
-    POSITIVE_Y_VERTICES[2],
-    POSITIVE_Y_VERTICES[3],
-
-    NEGATIVE_Y_VERTICES[0],
-    NEGATIVE_Y_VERTICES[1],
-    NEGATIVE_Y_VERTICES[2],
-    NEGATIVE_Y_VERTICES[3],
-
-    POSITIVE_Z_VERTICES[0],
-    POSITIVE_Z_VERTICES[1],
-    POSITIVE_Z_VERTICES[2],
-    POSITIVE_Z_VERTICES[3],
-
-    NEGATIVE_Z_VERTICES[0],
-    NEGATIVE_Z_VERTICES[1],
-    NEGATIVE_Z_VERTICES[2],
-    NEGATIVE_Z_VERTICES[3],
-];
-
-pub(crate) const CUBE_INDICES: [u32; 36] = [
-    0, 1, 2, 2, 3, 0, //front face
-    4, 5, 6, 6, 7, 4, //right face
-    8, 9, 10, 10, 11, 8, //back face
-    12, 13, 14, 14, 15, 12, //left face
-    16, 17, 18, 18, 19, 16, //top face
-    20, 21, 22, 22, 23, 20, //bottom face
-];
-
-// pub(crate) const SKYBOX_BACKPLANE_INDICES: [u32; 36] = [
-//     //front face
-//     0, 1, 2, 2, 3, 0, //right face
-//     3, 7, 2, 2, 6, 7, //back face
-//     4, 5, 6, 6, 7, 4, //left face
-//     4, 5, 1, 1, 0, 4, //top face
-//     4, 0, 3, 3, 7, 4, //bottom face
-//     1, 2, 6, 6, 5, 1,
-// ];
+pub(crate) const SKYBOX_BACKPLANE_INDICES: [u32;6] = [0,1,2,2,3,0];
